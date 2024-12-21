f77e5e332ae2adf1c84a9415df1ee0342c310566
arasan01 <developer@arasan01.com>
2024-12-22 02:44:48 +0900

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,16 @@ const app = new Hono()
 app.use(renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(
+    <>
+      <h1>Hello!</h1>
+      <div>
+        <a href="https://dist.j4uapp.com/appcast.xml">App RSS</a>
+        <br />
+        <a href="https://dist.j4uapp.com/J4uApp-0.1.0-Installer.dmg">Download</a>
+      </div>
+    </>
+  )
 })
 
 export default app
